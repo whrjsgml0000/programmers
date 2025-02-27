d25096d4476f1dcfd23c0de63d226b8986572a14
whrjsgml0000 <whrjsgml4515@naver.com>
2025-02-27 17:54:09 +0900

    택배 상자 꺼내기 : 완료

--- src/lv1/택배상자꺼내기/TakeOutTheDeliveryBox.java
+++ src/lv1/택배상자꺼내기/TakeOutTheDeliveryBox.java
@@ -1,9 +1,28 @@
 package lv1.택배상자꺼내기;
 
-class Solution{
+class Solution {
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        while(true){
+            if(getUpperBoxNumber(num, w) > n)
+                break;
+            answer++;
+            num = getUpperBoxNumber(num, w);
+        }
+        return answer + 1;
+    }
 
+    public int getUpperBoxNumber(int selfBoxNumber, int w) {
+        if (selfBoxNumber % w == 0){
+            return selfBoxNumber + 1;
+        }
+        return selfBoxNumber + (w - (selfBoxNumber % w)) * 2 + 1;
+    }
 }
 
 public class TakeOutTheDeliveryBox {
-
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(13,3,6));
+    }
 }
